88956d78d581508c7d8c5d0133eee19fdcde6fa1
Yura Taras <yura.taras@gmail.com>
2013-11-22 22:04:55 +0200

    Starting from scratch

---
+++ src/test/scala/OrderedList.scala
@@ -0,0 +1,13 @@
+package simulations
+
+import org.scalatest.FunSuite
+import org.scalatest.prop.Checkers
+import org.scalacheck._
+import Prop._
+import Gen._
+import Arbitrary._
+import scalaz._
+import Scalaz._
+import scalacheck.ScalazProperties._
+import scalacheck.ScalazArbitrary._
+import scalacheck.ScalaCheckBinding._
