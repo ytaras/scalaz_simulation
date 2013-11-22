49755d5704180d26ae59a54f0b671fb5d79ee39b
Yura Taras <yura.taras@gmail.com>
2013-11-22 23:37:37 +0200

    Added tests

--- src/main/scala/OrderedList.scala
+++ src/main/scala/OrderedList.scala
@@ -3,14 +3,16 @@ package simulations
 trait OrderedListComponent {
   import scalaz.Order
   import scalaz.Monoid
+  import scalaz.Equal
   import scalaz.syntax.std.list._
   import scalaz.std.list._
   import scalaz.syntax.foldable._
   import scalaz.syntax.order._
+  import scalaz.syntax.equal._
 
   private def addOrdered[T](vs: List[T], v: T)(implicit o: Order[T]): List[T] =
     vs match {
-      case x :: xs if v lt x => v :: addOrdered(xs, v)
+      case x :: xs if x lt v => x :: addOrdered(xs, v)
       case _ => v :: vs
     }
 
@@ -26,19 +28,23 @@ trait OrderedListComponent {
 
   object OrderedList {
     def empty[T](implicit o: Order[T]): OrderedList[T] = new OrderedList(List())
-    def apply[T](xs: T*)(implicit o: Order[T]): OrderedList[T] =
+    def apply[T](xs: List[T])(implicit o: Order[T]): OrderedList[T] =
       new OrderedList(xs.foldLeft(List[T]()) { addOrdered(_, _) })(o)
   }
 
-  implicit def orderedListMonoid[T](implicit o: Order[T]) =
-    new Monoid[OrderedList[T]] {
+  implicit def orderedListInstances[T](implicit o: Order[T], e: Equal[T]) =
+    new Monoid[OrderedList[T]] with Equal[OrderedList[T]] {
       override def zero = OrderedList.empty[T]
       override def append(v1: OrderedList[T], v2: => OrderedList[T]) =
         v1 merge v2
+      override def equal(v1: OrderedList[T], v2: OrderedList[T]) =
+        v1.l equals v2.l
     }
 
-  class OrderedList[T] private (private val l: List[T])(implicit val o: Order[T]) {
+  class OrderedList[T] private ( val l: List[T])(implicit val o: Order[T]) {
     def add(v: T) = new OrderedList(addOrdered(l, v))(o)
     def merge(other: OrderedList[T]) = new OrderedList(mergeOrdered(l, other.l))
+    def toList = l
+    override def toString = s"OrderedList: ${l}"
   }
 }
--- src/test/scala/OrderedList.scala
+++ src/test/scala/OrderedList.scala
@@ -11,3 +11,32 @@ import Scalaz._
 import scalacheck.ScalazProperties._
 import scalacheck.ScalazArbitrary._
 import scalacheck.ScalaCheckBinding._
+
+class OrderedListTest extends FunSuite with Checkers
+    with OrderedListComponent {
+
+  def isOrdered[T](x: OrderedList[T])(implicit o: Order[T]) =
+    x.toList.sliding(2) filter { _.size == 2 } map { x =>
+      x(0) ?|? x(1)
+    } forall { _ != Ordering.GT }
+
+  test("generated lists are ordered") {
+    check { x: OrderedList[Long] => isOrdered(x) }
+  }
+  test("merged lists are ordered") {
+    check { (x: OrderedList[Long], y: OrderedList[Long]) =>
+      isOrdered(x merge y)
+    }
+  }
+
+  test("laws are satisfied") {
+    monoid.laws[OrderedList[Long]].check
+    equal.laws[OrderedList[Long]].check
+  }
+
+  implicit def genOrdered: Arbitrary[OrderedList[Long]] =
+    Arbitrary {
+      arbitrary[List[Long]] map { OrderedList(_) }
+    }
+
+}
